43a6d10078842d3d83cc2311c55fe59c654231c6
jainayu <500060884@stu.upes.ac.in>
2018-09-24 22:06:17 +0530

    Event Registration layout

---
+++ app/src/main/java/com/example/user/events/EventRegistration.java
@@ -0,0 +1,9 @@
+package com.example.user.events;
+
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class EventRegistration {
+    EditText name,branch,year,sapid,whatsappno,phoneno,email;
+    Spinner yearofstudy;
+}
